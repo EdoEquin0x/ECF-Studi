c13c74e76e53e04e6379d3484577af3db96d8bc9
Nicolas Bourgois <69121197+EdoEquin0x@users.noreply.github.com>
2022-10-05 13:23:00 +0200

    Update databaseauth.php

--- Dependance/databaseauth.php
+++ Dependance/databaseauth.php
@@ -1,15 +1,13 @@
 
-
-
 <?php
 
 //PHP pour se connecter à la base de donnée
 
-$db="d80c661365db6t";
-$dbhost="ec2-34-247-72-29.eu-west-1.compute.amazonaws.com";
-$dbport=5432;
-$dbuser="mowiazajkntnhr";
-$dbpasswd="ffd483a06293628f00e1b86117ad30398ee27a0a9f9b48e1638e8ce347136b8c";
+$db="dbs8778128";
+$dbhost="db5010360043.hosting-data.io";
+$dbport=3306;
+$dbuser="dbu2313881";
+$dbpasswd="Nico0405*";
 
 $auth = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
 
